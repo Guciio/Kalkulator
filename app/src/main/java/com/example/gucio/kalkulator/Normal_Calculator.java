8e1187628ea24266b7d9d94230c73ddfdbd556db
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-03-15 18:22:38 +0100

    First init

---
+++ app/src/main/java/com/example/gucio/kalkulator/Normal_Calculator.java
@@ -0,0 +1,62 @@
+package com.example.gucio.kalkulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Normal_Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_calculator);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        if(id == R.id.normal){
+            Intent i = new Intent(getApplicationContext(), Normal_Calculator.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.advance) {
+            Intent i = new Intent(getApplicationContext(), Advance_Calculator.class);
+            startActivity(i);
+            return true;
+        }
+        if(id == R.id.about){
+            setContentView(R.layout.content_about);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            return true;
+        }
+        if(id == R.id.exit){
+            finish();
+            return false;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
