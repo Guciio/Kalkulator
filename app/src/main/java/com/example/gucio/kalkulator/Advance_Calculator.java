d79e21c198bdc8304305c3607c40b0a6b4d2a4e3
Maciej Gutenplan <202160@edu.p.lodz.pl>
2018-04-04 13:28:56 +0200

    Dodanie funk. normalCal do AdvanceCalc

--- app/src/main/java/com/example/gucio/kalkulator/Advance_Calculator.java
+++ app/src/main/java/com/example/gucio/kalkulator/Advance_Calculator.java
@@ -1,23 +1,331 @@
 package com.example.gucio.kalkulator;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Advance_Calculator extends AppCompatActivity {
+    private Button zero,one,two,three,four,five,six,seven,eight,nine,dod,udejm,mnoz,dziel,usun,rowna,kropka,cofnij,plusMinus;
+    private TextView wynik;
 
+    private double wartPierwsza;
+    private double wartDruga;
+
+    String tekstOkienko = "";
+    String odpowiedz = "";
+
+    protected int idOdpowiedzi = 0;
+    int operacjaPoRowna = 0;
+
+    void setupNormalCalculator(){
+        zero = (Button) findViewById(R.id.btn0);
+        one =(Button) findViewById(R.id.btn1);
+        two = (Button) findViewById(R.id.btn2);
+        three = (Button) findViewById(R.id.btn3);
+        four = (Button) findViewById(R.id.btn4);
+        five = (Button) findViewById(R.id.btn5);
+        six = (Button) findViewById(R.id.btn6);
+        seven = (Button) findViewById(R.id.btn7);
+        eight = (Button) findViewById(R.id.btn8);
+        nine = (Button) findViewById(R.id.btn9);
+        dod = (Button) findViewById(R.id.btnDod);
+        udejm = (Button) findViewById(R.id.btnMinus);
+        mnoz = (Button) findViewById(R.id.btnMnoz);
+        dziel = (Button) findViewById(R.id.btnDziel);
+        kropka = (Button) findViewById(R.id.btnKrop);
+        wynik = (TextView) findViewById(R.id.wynik);
+        usun = (Button) findViewById(R.id.btnC);
+        rowna = (Button) findViewById(R.id.btnRowna);
+        cofnij = (Button) findViewById(R.id.btnBksp);
+        plusMinus = (Button) findViewById(R.id.btnPlusMinus);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advance_calculator);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        setupNormalCalculator();
+        advanceCalculator();
 
     }
+
+    private void advanceCalculator()
+    {
+        zero.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "0");
+            }
+        });
+        one.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                wynik.setText(wynik.getText() + "1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                wynik.setText(wynik.getText() + "2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                wynik.setText(wynik.getText() + "6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {wynik.setText(wynik.getText() + "9");
+            }
+        });
+        dod.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                checkWhichMathOperationAndDo();
+
+                if(!tekstOkienko.isEmpty()) {
+                    wartPierwsza = Double.parseDouble(tekstOkienko);
+                    idOdpowiedzi = 1;
+                    tekstOkienko = "";
+                }
+            }
+
+        });
+        udejm.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                checkWhichMathOperationAndDo();
+                if(!tekstOkienko.isEmpty()) {
+
+                    wartPierwsza = Double.parseDouble(tekstOkienko);
+                    idOdpowiedzi = 2;
+                    tekstOkienko = "";
+                }
+            }
+        });
+        mnoz.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                checkWhichMathOperationAndDo();
+                if(!tekstOkienko.isEmpty()) {
+
+                    wartPierwsza = Double.parseDouble(tekstOkienko);
+                    idOdpowiedzi = 3;
+                    tekstOkienko = "";
+                }
+            }
+        });
+        dziel.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                checkWhichMathOperationAndDo();
+                if(!tekstOkienko.isEmpty()) {
+
+                    wartPierwsza = Double.parseDouble(tekstOkienko);
+                    idOdpowiedzi = 4;
+                    tekstOkienko = "";
+                }
+            }
+        });
+        kropka.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String tmp = tekstOkienko + ".";
+                try {
+                    Double.parseDouble(tmp);
+                    tekstOkienko = tmp;
+
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+                if(tekstOkienko.isEmpty()) tekstOkienko ="0.";
+                wynik.setText(tekstOkienko);
+
+            }
+        });
+
+        usun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tekstOkienko = "";
+                odpowiedz = "";
+                wynik.setText(tekstOkienko);
+                idOdpowiedzi = 0;
+                operacjaPoRowna = 0;
+                wartPierwsza= 0;
+                wartDruga = 0;
+            }
+        });
+
+        cofnij.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!tekstOkienko.isEmpty()) {
+                    tekstOkienko = tekstOkienko.substring(0, tekstOkienko.length() - 1);
+                    wynik.setText(tekstOkienko);
+                }
+            }
+        });
+
+        cofnij.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                tekstOkienko = "";
+                wynik.setText(tekstOkienko);
+                return true;
+            }
+        });
+
+        rowna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!tekstOkienko.isEmpty() && idOdpowiedzi == 0){
+                    odpowiedz = "";
+                    wartPierwsza = 0;
+                    wartDruga = 0;
+                    operacjaPoRowna = 0;
+                }
+
+                if(idOdpowiedzi != 0)
+                    operacjaPoRowna = idOdpowiedzi;
+
+                if (!tekstOkienko.isEmpty() && idOdpowiedzi != 0 ) {
+                    wartDruga = Double.parseDouble(tekstOkienko);
+                    tekstOkienko = "";
+                }
+
+                if(operacjaPoRowna != 0) {
+                    odpowiedz = doMathOperation(operacjaPoRowna);
+                    wynik.setText(odpowiedz);
+                    wartPierwsza = Double.parseDouble(odpowiedz);
+                    idOdpowiedzi = 0;
+                }
+            }
+        });
+
+        plusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!tekstOkienko.isEmpty()){
+                    Double d = Double.parseDouble(tekstOkienko);
+                    if(d != 0) tekstOkienko = String.valueOf(d * -1);
+                    wynik.setText(tekstOkienko);
+                }
+
+                else if(!odpowiedz.isEmpty()){
+                    wartPierwsza=wartPierwsza * -1;
+                    Double d = Double.parseDouble(odpowiedz);
+                    if(d != 0) odpowiedz = String.valueOf(d * -1);
+                    wynik.setText(odpowiedz);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    private String Add(double x, double y){
+        return String.valueOf(x + y);
+    }
+    private String Sub(double x, double y){
+        return String.valueOf(x - y);
+    }
+    private String Multi(double x, double y){
+        return String.valueOf(x * y);
+    }
+    private String Div(double x, double y){
+        return String.valueOf(x / y);
+    }
+
+
+    private void checkWhichMathOperationAndDo(){
+        if(idOdpowiedzi > 0 && !tekstOkienko.isEmpty())  {
+            wartDruga = Double.parseDouble(tekstOkienko);
+
+            odpowiedz = doMathOperation(idOdpowiedzi);
+            tekstOkienko = odpowiedz;
+            odpowiedz ="";
+            wynik.setText(tekstOkienko);
+        }
+
+        if(tekstOkienko.isEmpty() && !odpowiedz.isEmpty()) {
+            tekstOkienko = odpowiedz;
+            odpowiedz ="";
+            wartDruga = Double.parseDouble(tekstOkienko);
+        }
+    }
+
+    private String doMathOperation(int whichOperation){
+        switch (whichOperation) {
+            case 0:
+                break;
+            case 1:
+                return Add(wartPierwsza, wartDruga);
+            case 2:
+                return Sub(wartPierwsza, wartDruga);
+            case 3:
+                return Multi(wartPierwsza, wartDruga);
+            case 4:
+                if(wartPierwsza != 0)
+                    return Div(wartPierwsza, wartDruga);
+                else
+
+                    break;
+        }
+        return odpowiedz;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
